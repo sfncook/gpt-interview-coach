bca17767614edc481104c2da510ce0763b0defe5
Shawn Cook <sfncook@gmail.com>
2023-08-19 10:23:57 -0700

    ui for interview answer

--- pages/interview.js
+++ pages/interview.js
@@ -6,6 +6,8 @@ export default function Home() {
   const [jobTitleInput, setJobTitleInput] = useState("");
   const [jobDescInput, setJobDescInput] = useState("");
   const [questionResult, setQuestionResult] = useState();
+  const [interviewAnswerInput, setInterviewAnswerInput] = useState();
+  const [interviewAnswerEvaluationResult, setInterviewAnswerEvaluationResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -33,6 +35,43 @@ export default function Home() {
     }
   }
 
+  async function onSubmitInterviewAnswer(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/evaluateInterviewAnswer", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ jobTitle: jobTitleInput, jobDesc: jobDescInput, interviewAnswer: interviewAnswerInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setInterviewAnswerEvaluationResult(data.result);
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  const submitTxt = (questionResult) ? "Next question" : "Generate a sample interview question"
+  let interviewAnswerEl = (questionResult) ?
+    <form onSubmit={onSubmitInterviewAnswer}>
+      <textarea
+        name="interviewAnswer"
+        placeholder="Enter your response"
+        value={interviewAnswerInput}
+        onChange={(e) => setInterviewAnswerInput(e.target.value)}
+      />
+      <input type="submit" value="Submit your answer for evaluation" />
+    </form>
+    : <span/>
+
   return (
     <div>
       <Head>
@@ -40,8 +79,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Job title</h3>
+        <h3>Interview Coach</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
@@ -56,9 +94,11 @@ export default function Home() {
             value={jobDescInput}
             onChange={(e) => setJobDescInput(e.target.value)}
           />
-          <input type="submit" value="Generate sample interview question" />
+          <input type="submit" value={submitTxt} />
         </form>
         <div className={styles.result}>{questionResult}</div>
+        {interviewAnswerEl}
+        <div className={styles.result}>{interviewAnswerEvaluationResult}</div>
       </main>
     </div>
   );
